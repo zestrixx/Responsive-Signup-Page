45ea96714e5e13f9cda39c46bc677f74b98078d3
Mayank <mayank01.kv@gmail.com>
2021-10-31 18:53:22 +0530

    added login link and modified signup button

---
+++ Neumorphic signup page/signup.js
@@ -0,0 +1,6 @@
+$('.reg-button').click(function (e) { 
+    e.preventDefault();
+    $('.reg-button').css('border-radius', '50px');
+    $('.reg-button').css('background', 'linear-gradient(145deg, #6dd4c8, #81fdee)');
+    $('.reg-button').css('box-shadow', '10px 10px 20px #6dd4c8,-10px -10px 20px #85fff4');
+});
